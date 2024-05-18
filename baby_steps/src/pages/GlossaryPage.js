692ad0b1bc86e1a29c5a7b0f87b0663f5b7737f0
Emily He <emmyemmyhe@gmail.com>
2024-05-18 15:18:04 -0700

    connected pages to app

---
+++ baby_steps/src/pages/GlossaryPage.js
@@ -0,0 +1,12 @@
+function GlossaryPage() {
+
+
+    return (
+    <div className="glossarypage">
+      This is The Glossary Page.
+      </div>
+    );
+  }
+  
+  export default GlossaryPage;
+  
---
+++ baby_steps/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+function HomePage() {
+
+
+  return (
+  <div className="homepage">
+    This is The Homepage
+    </div>
+  );
+}
+
+export default HomePage;
