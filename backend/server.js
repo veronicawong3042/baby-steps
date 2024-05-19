2410e21e7c4373f62536374bbeba3a4154f66106
linda <llee20@students.cs.ubc.ca>
2024-05-18 19:15:20 -0700

    added fetch to question test page

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,47 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose()
+const cors = require('cors');
+
+const app = express();
 const port = 3000;
 
-function start() {
-    const express = require('express');
-    const sqlite3 = require('sqlite3').verbose();
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+};
 
-    const app = express();
+// Middleware
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
-    const database = new sqlite3.Database("./db/database.sqlite");
+const database = new sqlite3.Database("./db/database.sqlite");
 
-    //create table for api
-    database.run("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, answer TEXT)");
+//create table for api
+database.run("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, answer TEXT)");
 
-    //get is getting whatever data that already exists
-    app.get('/questions', (req, res) => {
-        //retrieve all users from the database
-        database.all('SELECT * FROM questions', (err, rows) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Internal Server Error');
-            } else {
-                res.send(rows);
-            }
-        });
+//get is getting whatever data that already exists
+app.get('/questions', (req, res) => {
+    //retrieve all users from the database
+    database.all('SELECT * FROM questions', (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json({
+                rows
+            });
+        }
     });
+});
 
-    app.post('/questions', (req, res) => {
-        database.run('INSERT INTO questions (question, answer) VALUES ("Which of the following statements about ovulation and fertility tracking is correct?", "Tracking ovulation methods include basal body temperature charting, ovulation predictor kits, and monitoring cervical mucus changes.")');
-        database.run('INSERT INTO questions (question, answer) VALUES ("What are the initial signs and symptoms of pregnancy?", "Early signs and symptoms of pregnancy may include missed periods, breast tenderness, nausea (morning sickness), fatigue, frequent urination, and heightened sense of smell.")');
-        res.send("successfully added questions!");
-    })
+app.post('/questions', (req, res) => {
+    database.run('INSERT INTO questions (question, answer) VALUES ("Which of the following statements about ovulation and fertility tracking is correct?", "Tracking ovulation methods include basal body temperature charting, ovulation predictor kits, and monitoring cervical mucus changes.")');
+    database.run('INSERT INTO questions (question, answer) VALUES ("What are the initial signs and symptoms of pregnancy?", "Early signs and symptoms of pregnancy may include missed periods, breast tenderness, nausea (morning sickness), fatigue, frequent urination, and heightened sense of smell.")');
+    res.send("successfully added questions!");
+})
 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
-}
-start();
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
 
--- frontend/src/pages/QuestionsTestPage.js
+++ frontend/src/pages/QuestionsTestPage.js
@@ -1,8 +1,33 @@
+import {useEffect, useState} from 'react';
+
 function QuestionsTestPage() {
+
+    const [rows, setRows] = useState([]);
+    const [ques, setQues] = useState({
+        0: false,
+        1: false,
+        2: false
+    })
+
+    useEffect(() => {
+        fetch("http://localhost:3000/questions", {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data.rows[0]);
+            setRows(data.rows);
+        })
+        .catch(err => console.log(err))
+    })
 
     return(
         <div>
-            This is the questions testing page for linda. no one but her will touch this file unless given permission.
+            {rows.map((item) => {
+                return <p>
+                    {item.id}:{item.question}:{item.answer}
+                </p>
+            })}
         </div>
     )
 }
