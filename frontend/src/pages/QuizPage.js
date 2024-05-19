1542051a6ed533b641f698c5215883d67780e832
Emily He <emmyemmyhe@gmail.com>
2024-05-18 20:45:34 -0700

    set up boiler plate content/placeholder for quiz questions

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -4,11 +4,27 @@ import { NavLink } from "react-router-dom";
 function QuizPage() {
   return (
     <div className="quiz-page">
-      This is the quiz navigation page in lessons. Connected to the lesson
-      component.
+      <h2>Question 1</h2>
       <NavLink to={`/`} className="close-button">
         <IoCloseSharp />
       </NavLink>
+      <blockquote>Why are we doing this hackathon?</blockquote>
+      <div className="option-container">
+        <input type="radio" id="option-1"></input>
+        <label for="option-1">Answer 1</label>
+
+        <input type="radio" id="option-2"></input>
+        <label for="option-2">Answer 2</label>
+
+        <input type="radio" id="option-3"></input>
+        <label for="option-3">Answer 3</label>
+
+        <input type="radio" id="option-4"></input>
+        <label for="option-4">Answer 4</label>
+      </div>
+
+      {/* Next Question Button ONLY SHOWS AFTER AN ANSWER HAS BEEN GIVEN */}
+      <button>Next Question</button>
     </div>
   );
 }
