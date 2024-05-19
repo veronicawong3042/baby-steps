6ea3b0f652e8b5a8de54a774afbb38d96d3d041c
Emily He <emmyemmyhe@gmail.com>
2024-05-18 17:33:39 -0700

    created and set up pages for both lessons interactions, components and for static pages within the application app management

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -1,23 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import { GlobalProvider } from "../context/GlobalContext";
+// import { GlobalProvider } from "../context/GlobalContext"; - Not needed but left here just in case
 // ------------------------ //
 import HomePage from "../pages/HomePage";
 import GlossaryPage from "../pages/GlossaryPage";
+import ChapterNavPage from "../pages/ChapterNavPage";
+import EducationPage from "../pages/EducationPage";
+import QuizPage from "../pages/QuizPage";
+import CongratsPage from "../pages/CongratsPage";
+// ------------------------ //
+// Below are for testing purposes only, not included in final product
 import QuestionsTestPage from "../pages/QuestionsTestPage";
 
 function AppRouter() {
-    return (
-      <BrowserRouter basename="/">
-        {/* <GlobalProvider> */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/glossary-page" element={<GlossaryPage />} />
-            <Route path="/questions-test-page" element={<QuestionsTestPage />} />
-          </Routes>
-        {/* </GlobalProvider> */}
-      </BrowserRouter>
-    );
-  }
-  
-  export default AppRouter;
-  
+  return (
+    <BrowserRouter basename="/">
+      {/* <GlobalProvider> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/glossary-page" element={<GlossaryPage />} />
+        <Route path="/chapter-navigation" element={<ChapterNavPage />} />
+
+        {/* Below are the Lesson related pages */}
+        <Route path="/education-page" element={<EducationPage />} />
+        <Route path="/quiz-page" element={<QuizPage />} />
+        <Route path="/congrats-page" element={<CongratsPage />} />
+
+        {/* Below are for testing purposes only, not included in final product */}
+        <Route path="/questions-test-page" element={<QuestionsTestPage />} />
+      </Routes>
+      {/* </GlobalProvider> */}
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
